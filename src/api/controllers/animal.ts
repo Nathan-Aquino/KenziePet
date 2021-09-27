d38ce09f90450cf52c58f73c3b6364b3d987737a
Nathan <nathan.ar.94@gmail.com>
2021-09-27 20:30:15 -0300

    feat: metodos de delecao, atualizacao, filtragem e listagem acrescentados

--- src/api/controllers/animal.ts
+++ src/api/controllers/animal.ts
@@ -4,6 +4,12 @@ import { Animal } from "../../entity/Animal";
 import { Group } from "../../entity/Group";
 import { Characteristic } from "../../entity/Characteristic";
 import characteristicsCreator from "../services/characteristicsCreator";
+import organizeAnimalResponse from "../services/organizeAnimalResponse";
+import {
+  AnimalType,
+  CharacteristicType,
+  GroupType,
+} from "../../types/typeAnimal";
 
 export const create = async (req: Request, res: Response) => {
   const animalRepository = getRepository(Animal);
@@ -52,4 +58,82 @@ export const create = async (req: Request, res: Response) => {
   };
 
   res.status(201).send(animalResponse);
+};
+
+export const list = async (req: Request, res: Response) => {
+  const animalRepository = getRepository(Animal);
+
+  let animalsFind = await animalRepository.find({
+    relations: ["group", "characteristics"],
+  });
+
+  const animalResponse = organizeAnimalResponse(animalsFind);
+
+  res.status(200).send(animalResponse);
+};
+
+export const filter = async (req: Request, res: Response) => {
+  const animalRepository = getRepository(Animal);
+
+  const id = req.params.animal_id;
+
+  const animal = (await animalRepository.findOne(id, {
+    relations: ["group", "characteristics"],
+  })) as AnimalType;
+
+  if (!animal) {
+    return res.status(404).send();
+  }
+
+  const animalResponse = organizeAnimalResponse(animal);
+
+  res.status(200).send(animalResponse);
+};
+
+export const updateAnimal = async (req: Request, res: Response) => {
+  const animalRepository = getRepository(Animal);
+  const groupRepository = getRepository(Group);
+
+  const id = req.params.animal_id;
+
+  let animal = await animalRepository.findOne(id);
+
+  if (!animal) return res.status(404).send();
+
+  let group = await groupRepository.findOne({ name: req.body.group.name });
+  if (!group) {
+    group = (await groupRepository.save(req.body.group)) as GroupType;
+  }
+
+  let characteristics = await characteristicsCreator(req.body.characteristics);
+
+  animal = {
+    id: animal.id,
+    name: req.body.name,
+    age: req.body.age,
+    weight: req.body.weight,
+    sex: req.body.sex,
+    group: group,
+    characteristics: characteristics,
+  };
+
+  animal = await animalRepository.save(animal);
+
+  const animalResponse = organizeAnimalResponse(animal);
+
+  res.status(200).send(animalResponse);
+};
+
+export const deleteAnimal = async (req: Request, res: Response) => {
+  const animalRepository = getRepository(Animal);
+
+  const id = req.params.animal_id;
+
+  const animal = await animalRepository.findOne(id);
+
+  if (!animal) return res.status(404).send();
+
+  await animalRepository.delete(id);
+
+  res.status(204).send();
 };
