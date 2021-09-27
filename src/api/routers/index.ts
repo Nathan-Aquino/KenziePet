b013f8e5331afafed4446b5e56c2c2ec28142f0f
Nathan <nathan.ar.94@gmail.com>
2021-09-26 23:27:09 -0300

    feat: funcao inicializadora das rotas dos animais adicionada

--- src/api/routers/index.ts
+++ src/api/routers/index.ts
@@ -1,10 +1,12 @@
 import { Express } from "express";
+import { initializerAnimalRoutes } from "./animal";
 import { initializerLoginRoute } from "./auth";
 import { initializerUserRoutes } from "./user";
 
 const initializerRoutes = (app: Express) => {
   initializerUserRoutes(app);
   initializerLoginRoute(app);
+  initializerAnimalRoutes(app);
 };
 
 export { initializerRoutes };
