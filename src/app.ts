cfc49a1dbeb1d7fe27754b8815b3fec6b0bed62a
Nathan <nathan.ar.94@gmail.com>
2021-09-26 23:28:01 -0300

    feat: inicializador da lib passport adicionado

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import "reflect-metadata";
 import express from "express";
 import { createConnection } from "typeorm";
+import passport from "passport";
 
 const app = express();
 app.use(express.json());
+app.use(passport.initialize());
 const PORT = 3000;
 
 import { initializerRoutes } from "./api/routers";
