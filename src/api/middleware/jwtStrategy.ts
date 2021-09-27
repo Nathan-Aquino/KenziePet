db8c59b3010acb02f36e023e3eccef7a0127a070
Nathan <nathan.ar.94@gmail.com>
2021-09-26 23:24:15 -0300

    fix: remofulado o corpo da funcao de estrategia

--- src/api/middleware/jwtStrategy.ts
+++ src/api/middleware/jwtStrategy.ts
@@ -2,31 +2,29 @@ import { Strategy, ExtractJwt, StrategyOptions } from "passport-jwt";
 import { User } from "../../entity/User";
 import { getRepository } from "typeorm";
 
-export const passportStrategy = (passport: any) => {
+export const passportStrategy = () => {
   const options: StrategyOptions = {
     secretOrKey: "xpto",
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
   };
 
-  const strategy = new Strategy(
-    options,
-    async (jwt_payload: any, done: any, err: any) => {
-      const userRepository = getRepository(User);
+  const verify = async (jwt_payload: any, done: Function, err: any) => {
+    const userRepository = getRepository(User);
 
-      const user = await userRepository.findOne(jwt_payload.id);
+    const user = await userRepository.findOne(jwt_payload.id);
 
-      if (user) {
-        const newUser = {
-          id: user.id,
-          username: user.username,
-          is_superuser: user.is_superuser,
-        };
+    if (user) {
+      const newUser = {
+        id: user.id,
+        username: user.username,
+        is_superuser: user.is_superuser,
+      };
 
-        return done(null, newUser);
-      } else {
-        return done(err, false, { message: "not found" });
-      }
+      return done(null, newUser);
+    } else {
+      return done(err, false);
     }
-  );
-  passport.use(strategy);
+  };
+
+  return new Strategy(options, verify);
 };
